8bc2618db160fb9e7abc5bb83276a27538debbb8
ossi-k <ossi.kohvakka@gmail.com>
2020-05-01 14:11:19 +0300

    Osa 1: Tehty tehtävä 1.4: kurssitiedot step 4

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -44,26 +44,29 @@ const Part = (props) => {
 
 const App = () => {
   const course = "Half Stack Application development"
-  const part1 = {
-    name: "Fundamentals of React",
-    excercises: 10
-  }
-  const part2 = {
-    name: "Using props to pass data",
-    excercises: 7
-  }
-  const part3 = {
-    name: "State of a component",
-    excercises: 14
-  }
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      excercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      excercises: 7
+    },
+    {
+      name: "State of a component",
+      excercises: 14
+    }
+  ]
+  
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} excercises1 = {part1.excercises}
-                part2 = {part2.name} excercises2 = {part2.excercises} 
-                part3 = {part3.name} excercises3 = {part3.excercises}/>
-      <Total total={part1.excercises + part2.excercises + part3.excercises} />
+      <Content part1={parts[0].name} excercises1 = {parts[0].excercises}
+                part2 = {parts[1].name} excercises2 = {parts[1].excercises} 
+                part3 = {parts[2].name} excercises3 = {parts[2].excercises}/>
+      <Total total={parts[0].excercises + parts[1].excercises + parts[2].excercises} />
     </div>
   )
 }
