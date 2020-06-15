5f05e64919f3187990bc9d92cf1fbfa58e08cc77
ossi-k <ossi.kohvakka@gmail.com>
2020-06-15 17:15:25 +0300

    Tehty osa 2b, tehtävä 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,19 +2,24 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
 
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     const duplicate = persons.find(person => personObject.name === person.name)
-    
+
     if (persons.includes(duplicate)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
@@ -28,6 +33,10 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
 
   return (
     <div>
@@ -38,18 +47,24 @@ const App = () => {
             onChange={handlePersonChange}
           />
         </div>
+        <div>number: <input value={newNumber}
+          onChange={handleNumberChange}
+        />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
+      <a>
         {persons.map(person =>
-          <li key={person.name}>
-            {person.name}
-          </li>
+          <a key={person.name}>
+            {person.name}, {" "}
+            {person.number}
+            <br/>
+          </a>
         )}
-      </ul>
+      </a>
     </div>
   )
 
