d01edbb4e4de5f46aa0d595de9045cd2e25c04f0
ossi-k <ossi.kohvakka@gmail.com>
2020-08-24 21:46:32 +0300

    tehty osa3: Tehtävä 3.1 puhelinluettelon backend step1

---
+++ osa3/puhelinluettelon_backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+let notes = [ {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+},
+{
+    name: "Ada Lovelace",
+    number: "39-44-532523",
+    id: 4
+},
+{
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+},
+{
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+},
+{
+    name: "Päivittyykö?",
+    number: "57817",
+    id: 5
+}
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
