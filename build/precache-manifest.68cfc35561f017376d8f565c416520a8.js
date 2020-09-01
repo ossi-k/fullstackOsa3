a6ac39724a8a9b661b4e9ee37d10d008b2da4fd5
ossi-k <ossi.kohvakka@gmail.com>
2020-09-01 17:52:52 +0300

    Osa 3: Tehty tehtävät 3.9 ja 3.10

---
+++ build/precache-manifest.68cfc35561f017376d8f565c416520a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc5303f9279c215af0f4c301a69f645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a669ba62c78ac270890b",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "dac093ef38089d6be13f",
+    "url": "/static/js/2.25e1d8b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25e1d8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a669ba62c78ac270890b",
+    "url": "/static/js/main.95c95e46.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
---
+++ build/precache-manifest.e4c2209ea75d64e505c44c795436fe9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7d3dc905f35310d96c8a5f1a7afd5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec4a2aa47d0814ebd20",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "dac093ef38089d6be13f",
+    "url": "/static/js/2.25e1d8b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25e1d8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec4a2aa47d0814ebd20",
+    "url": "/static/js/main.48897dc8.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
